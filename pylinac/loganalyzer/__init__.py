9b0bb479bdf00e16da672f8704a8a9d337ece6af
James Kerns <jkerns100@gmail.com>
2014-10-31 11:17:04 -0500

    Initial Commit for loganalyzer module

---
+++ pylinac/loganalyzer/__init__.py
@@ -0,0 +1,5 @@
+"""The pylinac Log Analyzer module is designed to read in and qualitatively and quantitatively analyze linac log files from Varian
+machines. Both Clinac-era (dynalogs) and TrueBeam-era (trajectory) logs are readable. The logs are read in according to the formats
+described in the DynaLog File Viewer & Reference Guide and the TrueBeam 1.5/2.0 Trajectory Log File Specification, both available at
+my.varian.com.
+"""
