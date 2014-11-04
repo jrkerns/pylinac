7d99ac47811daf4b125959e11650433e06573e3b
James Kerns <jkerns100@gmail.com>
2014-11-04 16:56:26 -0600

    moved sector mask to common functions

--- pylinac/common/common_functions.py
+++ pylinac/common/common_functions.py
@@ -474,3 +474,34 @@ def peak_detect(y, x=None, threshold=0, min_peak_width=10, delta=0, find_min_ins
             index += 1
 
     return peak_vals, peak_idxs
+
+
+def sector_mask(shape, centre, radius, angle_range):
+    """
+    Return a boolean mask for a circular sector. The start/stop angles in
+    `angle_range` should be given in clockwise order.
+    """
+
+    x, y = np.ogrid[:shape[0], :shape[1]]
+    cx, cy = centre
+    # tmin, tmax = np.deg2rad(angle_range)
+    tmin, tmax = angle_range
+
+    # ensure stop angle > start angle
+    if tmax < tmin:
+        tmax += 2 * np.pi
+
+    # convert cartesian --> polar coordinates
+    r2 = (x - cx) * (x - cx) + (y - cy) * (y - cy)
+    theta = np.arctan2(x - cx, y - cy) - tmin
+
+    # wrap angles between 0 and 2*pi
+    theta %= (2 * np.pi)
+
+    # circular mask
+    circmask = r2 <= radius * radius
+
+    # angular mask
+    anglemask = theta <= (tmax - tmin)
+
+    return circmask * anglemask
