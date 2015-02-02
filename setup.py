657b03e594249909b0ff1efb4d90e6fcf3f30c68
James Kerns <jkerns100@gmail.com>
2015-02-02 14:55:43 -0600

    setup changed to 1) include demo files and 2) drop the tests directory via manifest.in

--- setup.py
+++ setup.py
@@ -4,16 +4,15 @@ from setuptools import setup, find_packages
 # 1. Ensure version has incremented.
 # 2. To make life easy, in PyCharm, go to Tools/Run setup.py task.../sdist with command line option "upload" to push to PyPI
 
-__version__ = '0.2.0'
-__version_info__ = (0, 2, 0)
+__version__ = '0.2.1'
+__version_info__ = (0, 2, 1)
 
 
 setup(
     name='pylinac',
     version=__version__,
     packages=find_packages(),
-    # include_package_data=True,
-    package_data={'pylinac/demo_files': ['cbct/*', 'starshot/*', 'vmat/*']},
+    include_package_data=True,
     zip_safe=False,  # allows users to view files in egg/distribution
     url='https://github.com/jrkerns/pylinac',
     keywords='medical physics AAPM TG142 quality assurance starshot cbct vmat',
