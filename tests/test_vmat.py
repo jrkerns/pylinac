bbeeada5fc54b503bc0045ccb724d1de49e82bfc
James Kerns <jkerns100@gmail.com>
2015-05-13 10:41:51 -0500

    test reworked

--- tests/test_vmat.py
+++ tests/test_vmat.py
@@ -3,32 +3,78 @@ import unittest
 import os.path as osp
 
 from pylinac.vmat import VMAT
+from pylinac.core.geometry import Point
 
 
 _vmat_test_files_dir = osp.join(osp.dirname(__file__), 'test_files', 'VMAT')
 
 within_1 = partial(unittest.TestCase().assertAlmostEqual, delta=1)
+within_01 = partial(unittest.TestCase().assertAlmostEqual, delta=0.1)
 
 class Test_general(unittest.TestCase):
     """Generic tests for VMAT class."""
     def setUp(self):
         self.vmat = VMAT()
 
+    def test_demo_image_loads(self):
+        """Test the that demo images load properly."""
+        # shouldn't raise
+        self.vmat.load_demo_image('drgs')
+        self.vmat.load_demo_image('mlcs')
+
     def test_analyze_without_both_images_loaded(self):
         """Raise an error if both images aren't loaded when analyzing."""
+        self.assertRaises(AttributeError, self.vmat.analyze, 'mlcs')
+        self.vmat.load_demo_image('drgs')
+        self.vmat.analyze('drgs')  # shouldn't raise
+
+    def test_img_loaded_tags(self):
+        """Test the 'is_loaded' type tags."""
         self.assertFalse(self.vmat.open_img_is_loaded)
         self.assertFalse(self.vmat.dmlc_img_is_loaded)
-        self.assertRaises(AttributeError, self.vmat.analyze, 'drmlc')
         self.vmat.load_demo_image()
         self.assertTrue(self.vmat.open_img_is_loaded)
         self.assertTrue(self.vmat.dmlc_img_is_loaded)
 
+    def test_number_of_segments(self):
+        """Test that the right amount of segments are constructed based on the given test."""
+        self.vmat.load_demo_image()
+        self.vmat.analyze('drgs')
+        self.assertEqual(len(self.vmat.segments), 7)
+        self.vmat.analyze('mlcs')
+        self.assertEqual(len(self.vmat.segments), 4)
 
-class Test_DRGS_demo(unittest.TestCase):
+
+class VMATMixin:
+
+    def test_overall_passed(self, test):
+        self.vmat.analyze(test)
+        self.assertTrue(self.vmat.passed)
+
+    def test_img_inversion(self):
+        """Check that the demo images indeed get inverted."""
+        top_corner_before = self.vmat.image_open.array[:20, :20].mean()
+        self.vmat._check_img_inversion()
+        top_corner_after = self.vmat.image_open.array[:20, :20].mean()
+        self.assertNotEqual(top_corner_before, top_corner_after)
+
+    def test_segment_positions(self, segment_dict):
+        for key, value in segment_dict.items():
+            within_1(self.vmat.segments[key].center.x, value.x)
+            within_1(self.vmat.segments[key].center.y, value.y)
+
+    def test_segment_values(self, segment_dict):
+        for key, value in segment_dict.items():
+            within_01(self.vmat.segments[key].r_dev, value['r_dev'])
+            within_01(self.vmat.segments[key].r_corr, value['r_corr'])
+
+
+class Test_DRGS_demo(VMATMixin, unittest.TestCase):
     """Tests of the result values of the DRGS demo images."""
     def setUp(self):
         self.vmat = VMAT()
         self.vmat.load_demo_image('drgs')
+        self.vmat.settings.x_offset = 20
 
     def test_demo(self):
         """Run the demo; no errors should arise."""
@@ -36,143 +82,53 @@ class Test_DRGS_demo(unittest.TestCase):
 
     def test_overall_passed(self):
         """Test that the overall pass flag is true for default settings"""
-        self.vmat.analyze('drgs')
-        self.assertTrue(self.vmat.passed)
+        super().test_overall_passed('drgs')
 
-    def test_img_inversion(self):
-        """Check that the demo images indeed get inverted."""
-        top_corner_before = self.vmat.image_open.array[:20, :20].mean()
-        self.vmat._check_img_inversion()
-        top_corner_after = self.vmat.image_open.array[:20, :20].mean()
-        self.assertNotEqual(top_corner_before, top_corner_after)
-
-    def test_scaling(self):
-        """Test scaling values are 1; i.e. 384x512@150cmSID."""
-        SID_scale, img_scaling = self.vmat._calc_im_scaling_factors()
-        self.assertEqual(SID_scale, 1)
-        self.assertEqual(img_scaling.x, 1)
-        self.assertEqual(img_scaling.y, 1)
-
-    def test_segments(self):
+    def test_segment_positions(self):
         """Test various values of the Segments."""
         self.vmat.analyze('drgs')
+        segment_dict = {0: Point(161, 192), 4: Point(314, 192)}
+        super().test_segment_positions(segment_dict)
 
-        # test center locations
-        # ...segment 0
-        self.assertEqual(self.vmat.segments[0].center.x, 159)
-        self.assertEqual(self.vmat.segments[0].center.y, 192)
-        # ...segment 4
-        self.assertEqual(self.vmat.segments[4].center.x, 311)
-        self.assertEqual(self.vmat.segments[4].center.y, 192)
-
-        # test segment properties
-        self.assertAlmostEqual(self.vmat.segments[1].abs_mean_dev, 0.0315, delta=0.003)
-        self.assertAlmostEqual(self.vmat.segments[2].max_dev, -0.283, delta=0.003)
-        self.assertAlmostEqual(self.vmat.segments[5].mean_ratio, 1.003, delta=0.0005)
-        self.assertAlmostEqual(self.vmat.segments[6].min_dev, 0.420, delta=0.003)
-        self.assertAlmostEqual(self.vmat.segments[0].deviations.max(), 1.234, delta=0.005)
-
-    def test_samples(self):
-        """Test various property values of Samples."""
+    def test_segment_values(self):
         self.vmat.analyze('drgs')
+        segment_dict = {
+            0: {'r_dev': 0.965, 'r_corr': 101.85},
+            4: {'r_dev': -0.459, 'r_corr': 100.42},
+        }
+        super().test_segment_values(segment_dict)
 
-        # correct number (38 for Millenium MLC; 76 for HDMLC)
-        self.assertEqual(self.vmat.num_samples, 38)
-
-        # test some center locations
-        within_1(self.vmat.segments[0].samples[0].center.x, 159)
-        within_1(self.vmat.segments[0].samples[0].center.y, 15)
-
-        within_1(self.vmat.segments[2].samples[8].center.x, 235)
-        within_1(self.vmat.segments[2].samples[8].center.y, 92)
-
-        within_1(self.vmat.segments[6].samples[37].center.x, 390)
-        within_1(self.vmat.segments[6].samples[37].center.y, 368)
-
-        # test some sample ratios
-        self.assertAlmostEqual(self.vmat.segments[6].samples[29].ratio, 1.009, delta=0.0005)
-        self.assertAlmostEqual(self.vmat.segments[1].samples[3].ratio, 1.007, delta=0.0005)
-        self.assertAlmostEqual(self.vmat.segments[4].samples[10].ratio, 0.9985, delta=0.0005)
-        self.assertAlmostEqual(self.vmat.segments[5].samples[32].ratio, 1.0016, delta=0.0005)
-
-    def test_lower_tolerance(self):
-        self.vmat.analyze(test='drgs', tolerance=1)
-
-
-class Test_DRMLC_demo(unittest.TestCase):
+class Test_MLCS_demo(VMATMixin, unittest.TestCase):
     """Tests of the result values of the DRMLC demo images."""
 
     def setUp(self):
         self.vmat = VMAT()
-        self.vmat.load_demo_image('drmlc')
+        self.vmat.load_demo_image('mlcs')
 
     def test_demo(self):
-        self.vmat.run_demo_drmlc()
+        self.vmat.run_demo_mlcs()
 
     def test_overall_passed(self):
         """Test that the overall pass flag is true for default settings"""
-        self.vmat.analyze('drmlc')
-        self.assertTrue(self.vmat.passed)
+        super().test_overall_passed('mlcs')
 
-    def test_img_inversion(self):
-        """Check that the demo images indeed get inverted."""
-        top_corner_before = self.vmat.image_open.array[:20, :20].mean()
-        self.vmat._check_img_inversion()
-        top_corner_after = self.vmat.image_open.array[:20, :20].mean()
-        self.assertNotEqual(top_corner_before, top_corner_after)
-
-    def test_scaling(self):
-        """Test scaling values are 1; i.e. 384x512@150cmSID."""
-        SID_scale, img_scaling = self.vmat._calc_im_scaling_factors()
-        self.assertEqual(SID_scale, 1)
-        self.assertEqual(img_scaling.x, 1)
-        self.assertEqual(img_scaling.y, 1)
-
-    def test_segments(self):
+    def test_segment_positions(self):
         """Test various values of the Segments."""
-        self.vmat.analyze('drmlc')
+        self.vmat.analyze('mlcs')
+        segment_dict = {0: Point(170, 192), 2: Point(285, 192)}
+        super().test_segment_positions(segment_dict)
 
-        # test center locations
-        # ...segment 0
-        within_1(self.vmat.segments[0].center.x, 170)
-        within_1(self.vmat.segments[0].center.y, 192)
-        # ...segment 2
-        within_1(self.vmat.segments[2].center.x, 288)
-        within_1(self.vmat.segments[2].center.y, 192)
-
-        # test segment properties
-        self.assertAlmostEqual(self.vmat.segments[0].abs_mean_dev, 0.406, delta=0.003)
-        self.assertAlmostEqual(self.vmat.segments[1].max_dev, -0.273, delta=0.003)
-        self.assertAlmostEqual(self.vmat.segments[3].mean_ratio, 1.008, delta=0.0005)
-        self.assertAlmostEqual(self.vmat.segments[2].min_dev, -0.500, delta=0.003)
-        self.assertAlmostEqual(self.vmat.segments[0].deviations.max(), 0.497, delta=0.002)
-
-    def test_samples(self):
-        """Test various property values of Samples."""
-        self.vmat.analyze('drmlc')
-
-        # correct number (38 for Millennium MLC; 76 for HDMLC)
-        self.assertEqual(self.vmat.num_samples, 38)
-
-        # test some center locations
-        within_1(self.vmat.segments[0].samples[0].center.x, 170)
-        within_1(self.vmat.segments[0].samples[0].center.y, 15)
-
-        within_1(self.vmat.segments[2].samples[8].center.x, 288)
-        within_1(self.vmat.segments[2].samples[8].center.y, 92)
-
-        within_1(self.vmat.segments[3].samples[37].center.x, 345)
-        within_1(self.vmat.segments[3].samples[37].center.y, 368)
-
-        # test some sample ratios
-        self.assertAlmostEqual(self.vmat.segments[0].samples[29].ratio, 1.006, delta=0.0005)
-        self.assertAlmostEqual(self.vmat.segments[1].samples[3].ratio, 1.003, delta=0.0005)
-        self.assertAlmostEqual(self.vmat.segments[2].samples[10].ratio, 0.999, delta=0.0005)
-        self.assertAlmostEqual(self.vmat.segments[3].samples[32].ratio, 1.006, delta=0.0005)
+    def test_segment_values(self):
+        self.vmat.analyze('mlcs')
+        segment_dict = {
+            0: {'r_dev': 0.437, 'r_corr': 100.89},
+            2: {'r_dev': -0.405, 'r_corr': 100.04},
+        }
+        super().test_segment_values(segment_dict)
 
 
-class Test_DRMLC_105(unittest.TestCase):
-    """Tests of the result values of DRMLC images at 105cm SID."""
+class Test_MLCS_105(VMATMixin, unittest.TestCase):
+    """Tests of the result values of MLCS images at 105cm SID."""
 
     drmlc_105_open = osp.join(_vmat_test_files_dir, 'DRMLCopen-105-example.dcm')
     drmlc_105_dmlc = osp.join(_vmat_test_files_dir, 'DRMLCmlc-105-example.dcm')
@@ -184,69 +140,23 @@ class Test_DRMLC_105(unittest.TestCase):
 
     def test_overall_passed(self):
         """Test that the overall pass flag is true for default settings"""
-        self.vmat.analyze('drmlc')
-        self.assertTrue(self.vmat.passed)
+        super().test_overall_passed('mlcs')
 
-    def test_img_inversion(self):
-        """Check that the images indeed get inverted (applicable to most EPID images)."""
-        top_corner_before = self.vmat.image_open.array[:20, :20].mean()
-        self.vmat._check_img_inversion()
-        top_corner_after = self.vmat.image_open.array[:20, :20].mean()
-        self.assertNotEqual(top_corner_before, top_corner_after)
-
-    def test_scaling(self):
-        """Test scaling values."""
-        SID_scale, img_scaling = self.vmat._calc_im_scaling_factors()
-        # SID scale is 0.7; i.e. 150 (reference SID) / 105 (measured SID).
-        self.assertEqual(SID_scale, 0.7)
-        # image scales are 2.0; 768x1024 (measurement on AS1000) vs 384x512 (reference on AS500)
-        self.assertEqual(img_scaling.x, 2)
-        self.assertEqual(img_scaling.y, 2)
-
-    def test_segments(self):
+    def test_segment_positions(self):
         """Test various values of the Segments."""
-        self.vmat.analyze('drmlc')
+        self.vmat.analyze('mlcs')
+        segment_dict = {0: Point(391, 384), 2: Point(552, 384)}
+        super().test_segment_positions(segment_dict)
 
-        # test center locations
-        # ...segment 0
-        within_1(self.vmat.segments[0].center.x, 426)
-        within_1(self.vmat.segments[0].center.y, 384)
-        # ...segment 2
-        within_1(self.vmat.segments[2].center.x, 544)
-        within_1(self.vmat.segments[2].center.y, 384)
+    def test_segment_values(self):
+        self.vmat.analyze('mlcs')
+        segment_dict = {
+            0: {'r_dev': -0.040, 'r_corr': 100.83},
+            2: {'r_dev': -0.021, 'r_corr': 100.85},
+        }
+        super().test_segment_values(segment_dict)
 
-        # test segment properties
-        self.assertAlmostEqual(self.vmat.segments[0].abs_mean_dev, 0.098, delta=0.002)
-        self.assertAlmostEqual(self.vmat.segments[1].max_dev, 0.210, delta=0.003)
-        self.assertAlmostEqual(self.vmat.segments[3].mean_ratio, 1.001, delta=0.001)
-        self.assertAlmostEqual(self.vmat.segments[2].min_dev, -0.283, delta=0.004)
-        self.assertAlmostEqual(self.vmat.segments[0].deviations.max(), 0.252, delta=0.002)
-
-    def test_samples(self):
-        """Test various property values of Samples."""
-        self.vmat.analyze('drmlc')
-
-        # correct number (38 for Millennium MLC; 76 for HDMLC)
-        self.assertEqual(self.vmat.num_samples, 38)
-
-        # test some center locations
-        within_1(self.vmat.segments[0].samples[0].center.x, 426)
-        within_1(self.vmat.segments[0].samples[0].center.y, 136)
-
-        within_1(self.vmat.segments[2].samples[8].center.x, 544)
-        within_1(self.vmat.segments[2].samples[8].center.y, 243)
-
-        within_1(self.vmat.segments[3].samples[37].center.x, 601)
-        within_1(self.vmat.segments[3].samples[37].center.y, 632)
-
-        # test some sample ratios
-        self.assertAlmostEqual(self.vmat.segments[0].samples[29].ratio, 0.995, delta=0.0005)
-        self.assertAlmostEqual(self.vmat.segments[1].samples[3].ratio, 1.000, delta=0.0005)
-        self.assertAlmostEqual(self.vmat.segments[2].samples[10].ratio, 0.996, delta=0.0005)
-        self.assertAlmostEqual(self.vmat.segments[3].samples[32].ratio, 0.995, delta=0.0005)
-
-
-class Test_DRGS_105(unittest.TestCase):
+class Test_DRGS_105(VMATMixin, unittest.TestCase):
     """Tests of the result values of DRMLC images at 105cm SID."""
     drgs_105_open = osp.join(_vmat_test_files_dir, 'DRGSopen-105-example.dcm')
     drgs_105_dmlc = osp.join(_vmat_test_files_dir, 'DRGSmlc-105-example.dcm')
@@ -255,65 +165,22 @@ class Test_DRGS_105(unittest.TestCase):
         self.vmat = VMAT()
         self.vmat.load_image(self.drgs_105_open, 'open')
         self.vmat.load_image(self.drgs_105_dmlc, 'dmlc')
+        self.vmat.settings.x_offset = 20
 
     def test_overall_passed(self):
         """Test that the overall pass flag is true for default settings"""
-        self.vmat.analyze('drgs')
-        self.assertTrue(self.vmat.passed)
+        super().test_overall_passed('drgs')
 
-    def test_img_inversion(self):
-        """Check that the images indeed get inverted (applicable to most EPID images)."""
-        top_corner_before = self.vmat.image_open.array[:20, :20].mean()
-        self.vmat._check_img_inversion()
-        top_corner_after = self.vmat.image_open.array[:20, :20].mean()
-        self.assertNotEqual(top_corner_before, top_corner_after)
-
-    def test_scaling(self):
-        """Test scaling values."""
-        SID_scale, img_scaling = self.vmat._calc_im_scaling_factors()
-        # SID scale is 0.7; i.e. 150 (reference SID) / 105 (measured SID).
-        self.assertEqual(SID_scale, 0.7)
-        # image scales are 2.0; 768x1024 (measurement on AS1000) vs 384x512 (reference on AS500)
-        self.assertEqual(img_scaling.x, 2)
-        self.assertEqual(img_scaling.y, 2)
-
-    def test_segments(self):
+    def test_segment_positions(self):
         """Test various values of the Segments."""
         self.vmat.analyze('drgs')
+        segment_dict = {0: Point(371, 384), 2: Point(478, 384)}
+        super().test_segment_positions(segment_dict)
 
-        # test center locations
-        within_1(self.vmat.segments[0].center.x, 415)
-        within_1(self.vmat.segments[0].center.y, 384)
-
-        within_1(self.vmat.segments[2].center.x, 491)
-        within_1(self.vmat.segments[2].center.y, 384)
-
-        # test segment properties
-        self.assertAlmostEqual(self.vmat.segments[0].abs_mean_dev, 0.290, delta=0.002)
-        self.assertAlmostEqual(self.vmat.segments[1].max_dev, 0.389, delta=0.003)
-        self.assertAlmostEqual(self.vmat.segments[5].mean_ratio, 0.995, delta=0.001)
-        self.assertAlmostEqual(self.vmat.segments[2].min_dev, -0.018, delta=0.002)
-        self.assertAlmostEqual(self.vmat.segments[0].deviations.max(), 0.584, delta=0.002)
-
-    def test_samples(self):
-        """Test various property values of Samples."""
+    def test_segment_values(self):
         self.vmat.analyze('drgs')
-
-        # correct number (38 for Millennium MLC; 76 for HDMLC)
-        self.assertEqual(self.vmat.num_samples, 38)
-
-        # test some center locations
-        within_1(self.vmat.segments[0].samples[0].center.x, 415)
-        within_1(self.vmat.segments[0].samples[0].center.y, 136)
-
-        within_1(self.vmat.segments[2].samples[8].center.x, 491)
-        within_1(self.vmat.segments[2].samples[8].center.y, 243)
-
-        within_1(self.vmat.segments[6].samples[37].center.x, 646)
-        within_1(self.vmat.segments[6].samples[37].center.y, 632)
-
-        # test some sample ratios
-        self.assertAlmostEqual(self.vmat.segments[0].samples[29].ratio, 0.996, delta=0.0005)
-        self.assertAlmostEqual(self.vmat.segments[1].samples[6].ratio, 0.998, delta=0.0005)
-        self.assertAlmostEqual(self.vmat.segments[5].samples[23].ratio, 0.999, delta=0.0005)
-        self.assertAlmostEqual(self.vmat.segments[6].samples[37].ratio, 0.994, delta=0.0005)
+        segment_dict = {
+            0: {'r_dev': 0.780, 'r_corr': 102.43},
+            4: {'r_dev': -0.282, 'r_corr': 101.357},
+        }
+        super().test_segment_values(segment_dict)
