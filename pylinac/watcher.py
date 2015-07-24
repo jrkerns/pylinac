9fa9dbc935caa018d45ecf2f9a070c49064a2c11
James Kerns <jkerns100@gmail.com>
2015-07-24 13:55:44 -0500

    added a directory watcher and tests

---
+++ pylinac/watcher.py
@@ -0,0 +1,161 @@
+import time
+import logging
+import os.path as osp
+import sys
+import abc
+
+try:
+    from watchdog.observers import Observer
+    from watchdog.events import FileSystemEventHandler
+except ImportError:
+    raise ImportError("Watchdog must be installed to perform file watching.")
+
+from pylinac.starshot import Starshot
+from pylinac.picketfence import PicketFence
+from pylinac.cbct import CBCT
+from pylinac.log_analyzer import MachineLog
+from pylinac.vmat import VMAT
+
+logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
+
+class AnalyzeMixin:
+    """Mixin for processing files caught by the pylinac watcher."""
+    obj = 'class that analyzes'
+    keywords = ('',)
+    save_image_method = 'save_analyzed_image'
+    save_text_method = 'return_results'
+    args = {}
+
+    def __init__(self, path):
+        self.basepath = osp.splitext(path)[0]
+        self.instance = self.obj(path)
+        self.analyze()
+        self.save_image()
+        self.save_text()
+
+    @property
+    def img_filename(self):
+        return self.basepath + '.png'
+
+    @property
+    def txt_filename(self):
+        return self.basepath + '.txt'
+
+    def save_image(self):
+        method = getattr(self.instance, self.save_image_method)
+        method(self.img_filename)
+
+    def save_text(self):
+        method = getattr(self.instance, self.save_text_method)
+        with open(self.txt_filename, 'w') as txtfile:
+            txtfile.write(method())
+
+    @abc.abstractproperty
+    def analyze(self):
+        pass
+
+
+class AnalyzeStar(AnalyzeMixin):
+    obj = Starshot
+    keywords = ('star',)
+    args = {'tolerance': 1, 'radius': 0.8}
+
+    def analyze(self):
+        self.instance.analyze(tolerance=self.args['tolerance'], radius=self.args['radius'])
+
+
+class AnalyzePF(AnalyzeMixin):
+    obj = PicketFence
+    keywords = ('pf', 'picket')
+    args = {'tolerance': 0.5, 'action_tolerance': 0.3}
+
+    def analyze(self):
+        self.instance.analyze(tolerance=self.args['tolerance'], action_tolerance=self.args['action_tolerance'])
+
+
+class AnalyzeCBCT(AnalyzeMixin):
+    obj = CBCT.from_zip_file
+    keywords = ('cbct', 'ct')
+    args = {'hu tolerance': 40, 'scaling tolerance': 1}
+
+    def analyze(self):
+        self.instance.analyze(hu_tolerance=self.args['hu tolerance'], scaling_tolerance=self.args['scaling tolerance'])
+
+
+class AnalyzeVMAT(AnalyzeMixin):
+    obj = VMAT.from_zip
+    keywords = ('vmat', 'drgs', 'drmlc')
+    args = {'tolerance': 1.5}
+
+    def analyze(self):
+        self.instance.analyze(tolerance=self.args['tolerance'])
+
+
+class AnalyzeLog(AnalyzeMixin):
+    obj = MachineLog
+    keywords = ('',)
+    args = {'resolution': 0.1, 'distTA': 1, 'doseTA': 1, 'threshold': 10}
+    save_image_method = 'save_summary'
+
+    def analyze(self):
+        self.instance.fluence.gamma.calc_map(doseTA=self.args['doseTA'], distTA=self.args['distTA'],
+                                             resolution=self.args['resolution'], threshold=self.args['threshold'])
+
+    def save_text(self):
+        with open(self.txt_filename, 'w') as txtfile:
+            txtfile.write(self.instance.report_basic_parameters(False))
+
+
+def analysis_should_be_done(path):
+    """Return boolean of whether the file should be analysed, based on if the filename
+    has a keyword."""
+    path = osp.basename(path).lower()
+    for analysis_class in (AnalyzeStar, AnalyzeCBCT, AnalyzeVMAT, AnalyzeLog, AnalyzePF):
+        for keyword in analysis_class.keywords:
+            if (keyword in path) and not any(item in path for item in ('.png', '.txt')):
+                # more specific filtering of data by type
+                if analysis_class in (AnalyzeCBCT, AnalyzeVMAT):
+                    if path.endswith('.zip'):
+                        time.sleep(2)
+                        return True, analysis_class
+                elif analysis_class == AnalyzeLog:
+                    if path.endswith('.dlg') or path.endswith('.bin'):
+                        time.sleep(1)
+                        return True, analysis_class
+                else:
+                    time.sleep(1)
+                    return True, analysis_class
+    return False, None
+
+
+class FileAnalyzerEvent(FileSystemEventHandler):
+    """Handler for file events."""
+
+    def on_created(self, event):
+        full_file_path = osp.abspath(event.src_path)
+        # determine if file has keyword in it
+        do_analysis, analysis_class = analysis_should_be_done(full_file_path)
+        # process it if so
+        if do_analysis:
+            logging.info(full_file_path + " file found and will be analyzed...")
+            analysis_class(full_file_path)
+            logging.info(full_file_path + " was analyzed and now has an associated .txt and .png file")
+        else:
+            logging.info(full_file_path + " was added but was not deemed a file to be analyzed.")
+
+
+if __name__ == "__main__":
+    event_handler = FileAnalyzerEvent()
+    observer = Observer()
+    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    observer.schedule(event_handler, path, recursive=True)
+    logging.info("Pylinac now watching at " + osp.abspath(path))
+    observer.start()
+    while True:
+        time.sleep(1)
+    observer.join()
+
+
---
+++ tests/test_watcher.py
@@ -0,0 +1,56 @@
+import unittest
+import os
+import subprocess
+import time
+import os.path as osp
+import shutil
+import sys
+
+test_files_dir = osp.join(osp.dirname(osp.abspath(__file__)), 'test_files')
+watcher_dir = osp.join(test_files_dir, 'watcher')
+watcher_script = osp.join(osp.dirname(osp.dirname(osp.abspath(__file__))), 'pylinac', 'watcher.py')
+
+
+class WatcherTest(unittest.TestCase):
+    """Test the watcher script by adding a file from every type of module."""
+    files2analyze = [
+        osp.join(test_files_dir, 'Starshot', 'starshot_gantry.tif'),
+        osp.join(test_files_dir, 'Picket Fence', 'AS500_PF.dcm'),
+        osp.join(test_files_dir, 'VMAT', 'DRMLC.zip'),
+        osp.join(test_files_dir, 'CBCT', 'GE_CT.zip'),
+        osp.join(test_files_dir, 'MLC logs', 'tlogs', 'qqq2106_4DC Treatment_JS0_TX_20140712095629.bin')
+    ]
+
+    @classmethod
+    def setUpClass(cls):
+        """Setup the watcher."""
+        cls.watching_process = subprocess.Popen([sys.executable, watcher_script, watcher_dir])
+        time.sleep(1)
+
+    @classmethod
+    def tearDownClass(cls):
+        """Kill watcher and remove all files from the watcher director."""
+        cls.watching_process.kill()
+        time.sleep(1)
+        files = os.listdir(watcher_dir)
+        files.remove('dummy.txt')
+        for fyle in files:
+            fyle = osp.join(watcher_dir, fyle)
+            os.remove(fyle)
+
+    def test_all(self):
+        """Test all the files."""
+        for fyle in self.files2analyze:
+            self.process_file(fyle)
+
+    def process_file(self, filepath):
+        # copy the file over to the watcher directory
+        shutil.copy(filepath, watcher_dir)
+        new_path = osp.join(watcher_dir, osp.basename(filepath))
+
+        # wait for processing to finish by checking when the .txt file is generated
+        _, file_ext = osp.splitext(new_path)
+        safety_timer = 0
+        while not osp.isfile(new_path.replace(file_ext, '.txt')) and safety_timer < 30:
+            time.sleep(0.5)
+            safety_timer += 0.5
