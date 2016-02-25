13e5dd09ffec77753a802dd9095872f61ac4900c
James Kerns <jkerns100@gmail.com>
2016-02-25 17:04:58 -0600

    added docstrings and avoidance keywords to YAML config

--- pylinac/watcher.py
+++ pylinac/watcher.py
@@ -29,6 +29,14 @@ class AnalyzeMixin:
     ----------
     obj : class
         The class that analyzes the file; e.g. Starshot, PicketFence, etc.
+    config_name : str
+        The string that references the class in the YAML config file.
+    save_image_method : str
+        String-ified method name for saving the image to file.
+    save_test_method : str
+        String-ified method name for saving the text results to file.
+    expecting_zip : bool
+        Whether the class expects to find a ZIP archive, or normal file.
     """
     obj = object
     config_name = ''
@@ -37,12 +45,21 @@ class AnalyzeMixin:
     expecting_zip = False
 
     def __init__(self, path, config):
+        """
+        Parameters
+        ----------
+        path : str
+            The path to the file to be analyzed.
+        config :
+            The configuration settings of analysis. See `~pylinac.watcher.load_config`.
+        """
         self.full_path = path
         self.local_path = osp.basename(path)
         self.base_name = osp.splitext(self.local_path)[0]
         self.config = config
 
     def process(self):
+        """Process the file; includes analysis, saving results to file, and sending emails."""
         logging.info(self.local_path + " file found and will be analyzed...")
         self.instance = self.obj(self.full_path)
         self.analyze()
@@ -70,6 +87,7 @@ class AnalyzeMixin:
         return self.config[self.config_name]['keywords']
 
     def keyword_in_here(self):
+        """Determine whether a keyword exists in the filename."""
         if not self.expecting_zip:
             return any(keyword in self.local_path.lower() for keyword in self.keywords)
         else:
@@ -77,10 +95,12 @@ class AnalyzeMixin:
 
     @property
     def failure_settings(self):
+        """The YAML failure settings."""
         return self.config[self.config_name]['failure']
 
     @property
     def analysis_settings(self):
+        """The YAML analysis settings."""
         return self.config[self.config_name]['analysis']
 
     def send_email(self):
@@ -95,13 +115,11 @@ class AnalyzeMixin:
         if self.config['email']['enable-all']:
             statement = 'The pylinac watcher analyzed the file "{}" at {}. '
             statement += 'The analysis results are at "{}" and have also been attached here.'
-            statement = statement.format(self.local_path, current_time, osp.dirname(self.full_path))
         elif self.config['email']['enable-failure']:
             statement = 'The pylinac watcher analyzed the file "{}" at {} and '
             statement += 'found something that failed your configuration settings.'
             statement += 'The analysis results are at "{}" and have also been attached here.'
-            statement = statement.format(self.local_path, current_time,
-                                                                                                                                                             osp.dirname(self.full_path))
+        statement = statement.format(self.local_path, current_time, osp.dirname(self.full_path))
         # send the email
         contents = [statement, self.img_filename, self.txt_filename]
         yagserver = yagmail.SMTP(self.config['email']['sender'], self.config['email']['sender-password'])
@@ -123,15 +141,16 @@ class AnalyzeMixin:
             txtfile.write(method())
 
     def should_send_failure_email(self):
-        """Check whether analysis results were poor."""
+        """Check whether analysis results were poor and an email should be triggered."""
         return not self.instance.passed
 
     def analyze(self):
+        """Analyze the file."""
         self.instance.analyze(**self.analysis_settings)
 
 
 class AnalyzeWL(AnalyzeMixin):
-    """Analysis class for Winston-Lutz images."""
+    """Analysis runner for Winston-Lutz images."""
     obj = WinstonLutz.from_zip
     save_text_method = 'results'
     save_image_method = 'save_summary'
@@ -139,9 +158,11 @@ class AnalyzeWL(AnalyzeMixin):
     expecting_zip = True
 
     def analyze(self):
+        """Winson-Lutz doesn't explicitly analyze files."""
         pass
 
     def should_send_failure_email(self):
+        """Failure of WL is based on 3 criteria."""
         send = False
         for key, val in self.failure_settings:
             if key == 'gantry-iso-size':
@@ -157,89 +178,98 @@ class AnalyzeWL(AnalyzeMixin):
 
 
 class AnalyzeLeeds(AnalyzeMixin):
-    """Analysis class for Leeds TOR phantom."""
+    """Analysis runner for Leeds TOR phantom."""
     obj = LeedsTOR
     config_name = 'leeds'
 
 
 class AnalyzePipsPro(AnalyzeMixin):
-    """Analysis class for PipsPro QC-3."""
+    """Analysis runner for PipsPro QC-3."""
     obj = PipsProQC3
     config_name = 'pipspro'
 
 
 class AnalyzeStar(AnalyzeMixin):
-    """Analysis class for starshots."""
+    """Analysis runner for starshots."""
     obj = Starshot
     config_name = 'starshot'
 
 
 class AnalyzePF(AnalyzeMixin):
-    """Analysis class for picket fences."""
+    """Analysis runner for picket fences."""
     obj = PicketFence
     config_name = 'picketfence'
 
 
 class AnalyzeCBCT(AnalyzeMixin):
-    """Analysis class for CBCTs."""
+    """Analysis runner for CBCTs."""
     obj = CBCT.from_zip
     config_name = 'cbct'
     expecting_zip = True
 
     def should_send_failure_email(self):
+        """Failure of CBCT depends on individual module performance."""
         send = False
         for key, val in self.failure_settings:
-            if key == 'hu-passed':
-                if not self.instance.hu.overall_passed:
-                    send = True
-            if key == 'uniformity-passed':
-                if not self.instance.uniformity.overall_passed:
-                    send = True
-            if key == 'geometry-passed':
-                if not self.instance.geometry.overall_passed:
-                    send = True
-            if key == 'thickness-passed':
-                if not self.instance.thickness.passed:
-                    send = True
+            if key == 'hu-passed' and not self.instance.hu.overall_passed:
+                send = True
+            if key == 'uniformity-passed' and not self.instance.uniformity.overall_passed:
+                send = True
+            if key == 'geometry-passed' and not self.instance.geometry.overall_passed:
+                send = True
+            if key == 'thickness-passed' and not self.instance.thickness.passed:
+                send = True
         return send
 
 
 class AnalyzeVMAT(AnalyzeMixin):
-    """Analysis class for VMATs."""
+    """Analysis runner for VMATs."""
     obj = VMAT.from_zip
     config_name = 'vmat'
     expecting_zip = True
 
 
 class AnalyzeLog(AnalyzeMixin):
-    """Analysis class for dynalogs or trajectory logs."""
+    """Analysis runner for dynalogs or trajectory logs."""
     obj = MachineLog
     save_image_method = 'save_summary'
 
     def analyze(self):
+        """Log analysis is done via calculating gamma."""
         self.instance.fluence.gamma.calc_map(**self.analysis_settings)
 
     def keyword_in_here(self):
+        """Log keywords just check file extensions."""
         return self.local_path.endswith('.dlg') or self.local_path.endswith('.bin')
 
     def save_text(self):
+        """Special text save method."""
         with open(self.txt_filename, 'w') as txtfile:
             txtfile.write(self.instance.report_basic_parameters(False))
 
     def should_send_failure_email(self):
+        """Failure is based on several varying criteria."""
         send = False
-        gamma_too_low = self.instance.fluence.gamma.pass_prcnt < self.gamma_action_tolerance
-        rms_too_high = self.instance.mlc.get_RMS_percentile() > self.rms_action_tolerance
-        if gamma_too_low or rms_too_high:
-            send = True
+        for key, val in self.failure_settings:
+            if key == 'gamma':
+                gamma_below_threshold = self.instance.fluence.gamma.pass_prcnt < val
+                if gamma_below_threshold:
+                    send = True
+            elif key == 'avg-rms':
+                rms_above_threshold = self.instance.mlc.get_RMS_avg() > val
+                if rms_above_threshold:
+                    send = True
+            elif key == 'max-rms':
+                rms_above_threshold = self.instance.mlc.get_RMS_max() > val
+                if rms_above_threshold:
+                    send = True
         return send
 
 
 def analysis_should_be_done(path, config):
-    """Return boolean of whether the file should be analysed, based on if the filename
-    has a keyword."""
+    """Return boolean of whether the file should be analysed, based on if the filename has a keyword."""
     # return if not an analysis-worthy file
-    if any(item in path for item in ('.png', '.txt')):
+    if any(item in path for item in config['general']['avoid-keywords']):
         return False, None
     else:
         analysis_classes = (AnalyzeStar, AnalyzeCBCT, AnalyzeVMAT, AnalyzePF, AnalyzeWL, AnalyzeLog, AnalyzeLeeds, AnalyzePipsPro)
@@ -257,6 +287,7 @@ class FileAnalyzerEvent(FileSystemEventHandler):
         self.config = config
 
     def on_created(self, event):
+        """Called when a file is moved into the watched directory."""
         full_file_path = osp.abspath(event.src_path)
         local_path = osp.basename(full_file_path)
         # determine if file has keyword in it
