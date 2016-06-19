2462d539c8cbcb44ead5321e8d765d45dd83b257
James <jkerns100@gmail.com>
2016-06-18 20:47:39 -0500

    update final image classifier
    Former-commit-id: d7940da62acea2d6272b8109f19f9ecfb2a6abf0

--- machinelearning/singleimage/pylinac_learner.py
+++ machinelearning/singleimage/pylinac_learner.py
@@ -6,8 +6,8 @@ path = osp.join(osp.dirname(__file__), 'data')
 
 parameters = {
     'kernel': ['linear'],
-    'C': [1, 5],
+    'C': [1, 3, 0.3],
     # 'gamma': [0.001, 0.01, 0.1]
 }
 
-train(path, train_size=0.85, parameters=parameters, clf_name='singleimage')
+train(path, train_size=0.97, parameters=parameters, clf_name='singleimage')
