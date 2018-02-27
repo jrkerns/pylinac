35d5331f24da857c82ec74650a1e46ecc7143885
alanphys <alanphys@VMSTBOX.ONCOLOGY.COM>
2018-02-27 11:47:49 +0200

    Add Catphan604 datasets

--- pylinac/py_gui.py
+++ pylinac/py_gui.py
@@ -216,7 +216,7 @@ class PylinacGUI(Frame):
         Button(self.ct_tab, text='Load ZIP file...', command=load_zip).grid(column=2, row=3)
         Label(self.ct_tab, textvariable=self.ct_file).grid(column=2, row=4)
         Label(self.ct_tab, text='CatPhan type:').grid(column=2, row=5)
-        Combobox(self.ct_tab, values=('CatPhan504', 'CatPhan503', 'CatPhan600'), textvariable=self.ct_catphantype).grid(column=2, row=6)
+        Combobox(self.ct_tab, values=('CatPhan504', 'CatPhan503', 'CatPhan600', 'CatPhan604'), textvariable=self.ct_catphantype).grid(column=2, row=6)
         Label(self.ct_tab, text='HU Tolerance (HU):').grid(column=1, row=7)
         Entry(self.ct_tab, width=7, textvariable=self.ct_hu).grid(column=1, row=8)
         Label(self.ct_tab, text='Scaling tolerance (mm):').grid(column=2, row=7)
