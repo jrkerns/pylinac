07c765610ad5b4738c2a5e93bd287f34194b86d0
James Kerns <jkerns100@gmail.com>
2018-10-24 10:07:31 -0500

    further testing tweaks

--- test_bank.py
+++ test_bank.py
@@ -1,12 +1,8 @@
 """Test the entire package; an underscore precedes this file name
 so it does not include itself in the test discovery."""
 import os.path as osp
-from test_basic import run_tests
+from tests_basic import run_tests
 
-import matplotlib
-
-
-matplotlib.use('Agg')
 
 test_dir = osp.join(osp.dirname(__file__), 'tests_bank')
 run_tests(test_dir, pattern='_test*.py')
--- test_basic.py
+++ test_basic.py
@@ -1,23 +1,8 @@
 """Test the entire package; an underscore precedes this file name
 so it does not include itself in the test discovery."""
 import os.path as osp
-import unittest
-
-import matplotlib
+from tests_basic import run_tests
 
 
-matplotlib.use('Agg')
-
-
-def run_tests(directory, pattern='test_*.py'):
-    # import a runner to run tests_basic
-    runner = unittest.TextTestRunner()
-    # run test discovery
-    test_suite = unittest.defaultTestLoader.discover(directory, pattern)
-    # run test runner
-    runner.run(test_suite)
-
-
-if __name__ == '__main__':
-    test_dir = osp.join(osp.dirname(__file__), 'tests_basic')
-    run_tests(test_dir)
+test_dir = osp.join(osp.dirname(__file__), 'tests_basic')
+run_tests(test_dir)
