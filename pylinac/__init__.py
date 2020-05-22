25ba0ff72ccfbd5517a44df48d9f841fbc15e2e6
jrkerns <jkerns100@gmail.com>
2020-05-22 13:57:08 -0500

    Closes #284

--- pylinac/__init__.py
+++ pylinac/__init__.py
@@ -22,4 +22,3 @@ from pylinac.winston_lutz import WinstonLutz
 from pylinac.calibration import tg51, trs398
 
 from pylinac.watcher import process
-from pylinac.py_gui import gui  # must be after everything since it imports it all
