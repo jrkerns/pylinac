03a19cb28bf6124f19cff54a496fe2d192a094b4
James Kerns <jrkerns@users.noreply.github.com>
2021-04-14 09:29:03 -0500

    Closes #365 Adds a DLG measurement analysis.

---
+++ tests_basic/test_dlg.py
@@ -0,0 +1,14 @@
+import unittest
+from pathlib import Path
+
+from pylinac.dlg import DLG
+from pylinac.picketfence import MLCs
+
+
+class TestDLG(unittest.TestCase):
+    file_path = Path(__file__).parent / Path('test_files/DLG 1.5+0.2.dcm')
+
+    def test_measured_dlg(self):
+        dlg = DLG(self.file_path)
+        dlg.analyze(gaps=(-0.9, -1.1, -1.3, -1.5, -1.7, -1.9), mlc=MLCs['Millennium'])
+        self.assertAlmostEqual(dlg.measured_dlg, 1.503, delta=0.001)
