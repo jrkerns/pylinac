7c9ffcb39cdcc5fb18acb9c3b1e1995150f82ac9
James Kerns <jrkerns@users.noreply.github.com>
2023-01-31 13:40:58 -0600

    update kraken on pylinac update

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,5 @@
+import os
+
 import nox
 
 
@@ -15,3 +17,14 @@ def serve_docs(session):
 @nox.session(python=False)
 def build_docs(session):
     session.run("sphinx-build", "docs/source", "docs/build")
+
+
+@nox.session(python=False)
+def update_dev_kraken(session):
+    """Run the Kraken build to update it with new pylinac changes"""
+    key_info = os.environ['GOOGLE_CREDENTIALS']
+    with open("service_key.json", "w") as key_file:
+        key_file.write(key_info)
+    session.run("gcloud", "auth", "activate-service-account", "--key-file service_key.json")
+    session.run("gcloud", "config", "set", "project", 'radmachine')
+    session.run("gcloud", "builds", "triggers", "run", "kraken-build", "--branch=master")
