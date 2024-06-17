cb496c4cba533486828990399b00148ee9d14d70
James Kerns <jkerns@radformation.com>
2024-06-17 15:11:58 -0500

    final touches

--- tests_basic/core/test_generator.py
+++ tests_basic/core/test_generator.py
@@ -7,6 +7,7 @@ import numpy as np
 import numpy.testing
 import pydicom
 
+import tests_basic  # noqa; load env settings and filters
 from pylinac import Interpolation, Normalization
 from pylinac.core.image import DicomImage, load
 from pylinac.core.image_generator import (
