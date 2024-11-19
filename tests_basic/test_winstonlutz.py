ab9d306a752b01ae89d04a1ec9ecfbf9512d7970
James Kerns <jkerns@radformation.com>
2024-11-19 16:20:48 -0600

    delete the instance

--- tests_basic/test_winstonlutz.py
+++ tests_basic/test_winstonlutz.py
@@ -989,6 +989,7 @@ class WinstonLutzMixin(CloudFileMixin):
     @classmethod
     def tearDownClass(cls):
         plt.close("all")
+        del cls.wl
         super().tearDownClass()
 
     def test_number_of_images(self):
