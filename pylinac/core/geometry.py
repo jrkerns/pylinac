f2df24f14de09fd1a7db3b1f54319399bf0862b5
Joao Silveira <jsilveira@radformation.com>
2025-07-04 14:57:02 +0100

    Unrelated: in Rectangle, separated text from ASCII diagram since it does not work for the web documentation.

--- pylinac/core/geometry.py
+++ pylinac/core/geometry.py
@@ -553,6 +553,9 @@ class Rectangle:
     This always assumes an image/screen coordinate system where +x is to the right and +y is down.
     Thus, the "top-left" corner are colloquial terms and is the point with the lowest x and y coordinate values of the UNROTATED rectangle.
     From the users' perspective, it is the upper-most and left-most corner.
+    """
+
+    """
 
          ¦                                                ¦
          ¦                                                ¦
